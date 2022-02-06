5f38bf9080ca7195d8ecbeacb9470f39b6a38b2b
Rasl174 <98830529+Rasl174@users.noreply.github.com>
2022-02-06 09:27:07 +0500

    Добавьте файлы проекта.

---
+++ CS_DZ1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_DZ1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int speed;
+            bool canOpenFirstDoor;
+            string playerName;
+            byte helth;
+            float frictionForce;
+            char a, b, c, d;
+            object cube;
+            short maxSpeed;
+            long distanceToMars;
+            double brakingForce;
+
+        }
+    }
+}
